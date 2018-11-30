03d0b7a3f21b7e4f64f2bdaec774abbda131d951
SalvatiRyerson <45499011+SalvatiRyerson@users.noreply.github.com>
2018-11-30 16:45:49 -0500

    Add files via upload

---
+++ src/components/pages/tutorial.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Tutorial extends Component {
+  render() {
+    return (
+      <div className="headline">
+        <h1>Tutorial Content</h1>
+        <p>Tutorial Content Here</p>
+      </div>
+    );
+  }
+}
+
+export default Tutorial;
