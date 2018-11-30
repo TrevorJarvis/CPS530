ce20d811d11bb859469022f824b375d46fda5696
Phuoc Tran <phuocpeter19@gmail.com>
2018-11-30 18:37:01 -0500

    Add profile component

---
+++ client/src/components/creditsComponent/profile.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import placeholder from '../../placeholder.svg';
+
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'name': props.name,
+      'picUrl': props.picUrl ? props.picUrl : placeholder,
+      'desc': props.desc
+    }
+  }
+
+  render() {
+    return (
+    <div className="profile card" style={{width: "18rem"}}>
+      <img className="card-img-top" src={this.state.picUrl} alt={this.state.name}/>
+      <div className="card-body">
+        <h4 className="mt-0">{this.state.name}</h4>
+        {this.state.desc}
+      </div>
+    </div>
+  )}
+}
+
+export default Profile;
