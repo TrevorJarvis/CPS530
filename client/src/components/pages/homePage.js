950cc858f5637d0ed5e2c8dfea0b037f683d5805
Kevin Duong <kevinduong416@gmail.com>
2018-12-02 15:18:33 -0500

    Update homePage.js

--- client/src/components/pages/homePage.js
+++ client/src/components/pages/homePage.js
@@ -9,7 +9,7 @@ class Homepage extends Component {
           <h2>ReactJS</h2>
           <hr />
           <br />
-          <h3>Populary/Strengths</h3>
+          <h3>Popularity/Strengths</h3>
           <p>
             Very easy to use and very easy to learn. Being based on JavaScript,
             anyone with a moderate amount of understanding of how JavaScript
@@ -51,7 +51,48 @@ class Homepage extends Component {
             The open source community is relatively new and therefore small in
             comparison to some other communities.{" "}
           </p>
-        </div>
+		  <h2>ExpressJS</h2>
+		  <hr />
+		  <br />
+		  <h3>Popularity</h3>
+		  <p>
+			Express.JS is framework built based on Node.JS. Express.JS is built to 
+			assist on routing, creating a server, parsing, etc. It makes all these 
+			tasks much more simple to do. It is also very versatile and allows ease
+			of adding middlewares.
+		  </p>
+		  <p>
+			Because of these features, Express.JS is the one most popular web 
+			application framework of Node.JS.
+		  </p>
+		  <br />
+		  <h3>Strengths</h3>
+		  <p>
+			As a framework of Node.JS, it offers on what Node.JS brings. It is very 
+			easy to learn if the user already knows JavaScript which is also one of
+			the most popular among programming languages.
+		  </p>
+		  <p>
+			Express.JS has friendly integration of middleware and third party services.
+			Express.JS is designed to integrate these middleware with ease and solve
+			different development problems. The choice of middleware is very flexible
+			and the user is able to choose which any they want to solve any problem.
+		  </p>
+		  <p>
+			Express.JS is one the most mature Node.Js frameworks, so the open source
+			community has a decent size.
+		  </p>
+		  <br />
+		  <h3>Weaknesses</h3>
+		  <p>
+			Express.JS will also come with the disadvantages of Node.JS. Node.JS is a
+			single threaded framework, so it is prone to "callback hell." This will
+			cause some performance issues on heavy computation systems. If users
+			decide on using heavy-computing apps, it is recommended to use other
+			framework that does not rely on Node.JS.
+		  </p>
+		  <br />
+        </div>		
       </div>
     );
   }
